ef47b1943bc9e52c9b5a0ca2017164df394e4947
Davi Melo Cardozo <dmelo-ca@c1r13s6.42porto.com>
2024-05-19 18:19:59 +0100

    .

--- v2/Client.c
+++ v2/Client.c
@@ -2,6 +2,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
+
+void string_to_bits(const char *str, char *bits) {
+    int i = 0;
+    while (str[i] != '\0') {
+        char ch = str[i];
+        int j = 7;
+        while (j >= 0) {
+            if ((ch >> j) & 1) {
+                bits[i * 8 + (7 - j)] = '1';
+            } else {
+                bits[i * 8 + (7 - j)] = '0';
+            }
+            j--;
+        }
+        i++;
+    }
+    bits[i * 8] = '\0';
+}
+
+void sendbits(pid_t pid, const char *bits)
+{
+    int i;
+    int erro;
+
+    i = 0;
+    while (bits[i] != '\0')
+    {
+        if (bits[i] == '1')
+            erro = kill(pid, SIGUSR1);
+        else
+            erro = kill(pid, SIGUSR2);
+
+        if (erro == -1)
+            printf("erro");
+        i++;
+        usleep(1000);
+    }
+    printf("Sinal enviado!");
+    return ;
+}
 
 void handle_sigusr1(int sig) {
     printf("1");
@@ -12,23 +53,24 @@ void handle_sigusr2(int sig) {
 }
 
 int main(int ac, char **av) {
-    struct sigaction sa;
+/*     struct sigaction sa;
 
     sa.sa_handler = handle_sigusr1;
     sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
+    sa.sa_flags = SA_RESTART;
     sigaction(SIGUSR1, &sa, NULL);
 
     sa.sa_handler = handle_sigusr2;
     sigaction(SIGUSR2, &sa, NULL);
-    
+     */
     (void)ac;
     pid_t pid;
+    char bits[8 * (strlen(av[2]) + 1)];
     pid = atoi(av[1]);
     printf("PID: %d\n", pid);
+    string_to_bits(av[2], bits);
+    printf("%s\n", bits);
+    sendbits(pid, bits);
 
-    while (1) {
-        kill(pid, SIGUSR1);
-    }
     return 0;
 }
--- v2/Server.c
+++ v2/Server.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <sched.h>
 
 //int index = 0;
 
@@ -11,13 +10,14 @@ void    handle_sigusr1(int sig)
     //bits[index] = '1';
     //index++;
     printf("1");
+    write(1, "1", 1);
 }
 
 void    handle_sigusr2(int sig)
 {
     //bits[index] = '0';
     //index++;
-    printf("0");
+    write(1, "0", 1);
 }
 
 int main(void)
@@ -26,7 +26,7 @@ int main(void)
 
     sa.sa_handler = handle_sigusr1;
     sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
+    sa.sa_flags = SA_RESTART;
     if (sigaction(SIGUSR1, &sa, NULL) == -1) {
         perror("sigaction");
         return 1;
@@ -38,14 +38,12 @@ int main(void)
         return 1;
     }
 
+
     pid_t pid;
     //char bits[1024];
-
     pid = getpid();
     printf("PID: %d\n", pid);
-    while(1)
-    {
+    while (1)
         pause();
-    }
     return 0;
 }

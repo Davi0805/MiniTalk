4d0afc789f2a6564c7b95fcf09d7e11cb63c2016
Davi Melo Cardozo <dmelo-ca@c1r10s6.42porto.com>
2024-05-28 19:01:53 +0100

    .

--- v2/Client.c
+++ v2/Client.c
@@ -1,84 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmelo-ca <dmelo-ca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 17:58:10 by dmelo-ca          #+#    #+#             */
+/*   Updated: 2024/05/28 18:15:41 by dmelo-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <string.h>
 #include "./ft_printf/ft_printf.h"
 
-void string_to_bits(const char *str, int pid) {
-    int i = 0;
-    int j = 0;
-    while (str[i] != '\0') {
-        char ch = str[i];
-        int j = 7;
-        while (j >= 0) {
-            if ((ch >> j) & 1) {
-                kill(pid, SIGUSR1);
-            } else {
-                kill(pid, SIGUSR2);
-            }
-            j--;
-            usleep(1000);
-        }
-        i++;
-    }
-    while (j < 4)
-    {
-        kill(pid, SIGUSR2);
-        usleep(1000);
-        j++;
-    }
-        while (j > 2)
-    {
-        kill(pid, SIGUSR1);
-        usleep(1000);
-        kill(pid, SIGUSR2);
-        j--;
-    }
-}
-
-/* void sendbits(pid_t pid, const char *bits)
+void	string_to_bits(const char *str, int pid)
 {
-    int i;
-    int erro;
+	int		i;
+	int		j;
+	char	ch;
 
-    i = 0;
-    while (bits[i] != '\0')
-    {
-        if (bits[i] == '1')
-            erro = kill(pid, SIGUSR1);
-        else
-            erro = kill(pid, SIGUSR2);
-    //printf("Sinal enviado!");
-    return ;
-} */
-
-void handle_sigusr1(int sig) {
-    ft_printf("Erro");
+	ch = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ch = str[i];
+		j = 7;
+		while (j-- >= 0)
+		{
+			if ((ch >> j) & 1)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(250);
+		}
+		i++;
+	}
+	endofstring(pid);
 }
 
-void handle_sigusr2(int sig) {
-    ft_printf("Mensagem recebida");
+void	endofstring(int pid)
+{
+	int	j;
+
+	j = 0;
+	while (j++ < 4)
+	{
+		kill(pid, SIGUSR2);
+		usleep(250);
+	}
+	while (j-- > 2)
+	{
+		kill(pid, SIGUSR1);
+		usleep(250);
+		kill(pid, SIGUSR2);
+	}
 }
 
-int main(int ac, char **av) {
-    struct sigaction sa;
+void	handle_sigusr1(int sig)
+{
+	ft_printf("Erro");
+}
 
-    sa.sa_handler = handle_sigusr1;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    sigaction(SIGUSR1, &sa, NULL);
+void	handle_sigusr2(int sig)
+{
+	ft_printf("Mensagem recebida");
+}
 
-    sa.sa_handler = handle_sigusr2;
-    sigaction(SIGUSR2, &sa, NULL);
-    
-    if (ac != 3)
-        ft_printf("Numero de argumetos insuficiente!\n./Client <PID> <Mensagem>");
-    int pid;
-    pid = atoi(av[1]);
-    //ft_printf("PID: %d\n", pid);
-    string_to_bits(av[2], pid);
-//    sendbits(pid, bits);
+int	main(int ac, char **av)
+{
+	int					pid;
+	struct sigaction	sa;
 
-    return 0;
+	sa.sa_handler = handle_sigusr1;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &sa, NULL);
+	sa.sa_handler = handle_sigusr2;
+	sigaction(SIGUSR2, &sa, NULL);
+	if (ac != 3)
+		ft_printf("Numero de argumetos insuficiente!\n");
+	pid = atoi(av[1]);
+	string_to_bits(av[2], pid);
+	return (0);
 }
--- v2/Server.c
+++ v2/Server.c
@@ -1,56 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmelo-ca <dmelo-ca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 17:58:15 by dmelo-ca          #+#    #+#             */
+/*   Updated: 2024/05/28 18:17:24 by dmelo-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include "./ft_printf/ft_printf.h"
 
-void    bin_to_char(int signum, char *c)
+void	bin_to_char(int signum, char *c)
 {
-    if (signum == SIGUSR1)
-        *c = (*c << 1) | 1;
-    else if (signum == SIGUSR2)
-        *c <<= 1;
+	if (signum == SIGUSR1)
+		*c = (*c << 1) | 1;
+	else if (signum == SIGUSR2)
+		*c <<= 1;
 }
 
-void    handle_sigusr(int signum, siginfo_t *info, void *context)
+void	handle_sigusr(int signum, siginfo_t *info, void *context)
 {
-    static int  pid;
-    static int  bit_index;
-    static char c;
+	static int	pid;
+	static int	bit_index;
+	static char	c;
 
-    pid = info->si_pid;
-    bin_to_char(signum, &c);
-    if (++bit_index == 8)
-    {
-        bit_index = 0;
-        if (!c)
-        {
-            kill(pid, SIGUSR1);
-            pid = 0;
-            return ;
-        }
-        ft_printf("%c", c);
-        c = 0;
-    }
+	pid = info->si_pid;
+	bin_to_char(signum, &c);
+	if (++bit_index == 8)
+	{
+		bit_index = 0;
+		if (!c)
+		{
+			kill(pid, SIGUSR1);
+			pid = 0;
+			return ;
+		}
+		ft_printf("%c", c);
+		c = 0;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    struct sigaction sa;
-    sa.sa_flags = SA_RESTART | SA_SIGINFO;
-    sa.sa_sigaction = handle_sigusr;
-    sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
-        perror("sigaction");
-        return 1;
-    }
+	struct sigaction	sa;
 
-    if (sigaction(SIGUSR2, &sa, NULL) == -1) {
-        perror("sigaction");
-        return 1;
-    }
-    ft_printf("PID: %d\n", getpid());
-    while (1)
-        pause();
-    return 0;
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
+	sa.sa_sigaction = handle_sigusr;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		return (1);
+	}
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		perror("sigaction");
+		return (1);
+	}
+	ft_printf("PID: %d\n", getpid());
+	while (1)
+		pause();
+	return (0);
 }
